3945fea05b8f18a89ee9d5afc67cbd9613666d73
Soroush <s.ghodrat@gmail.com>
2024-12-05 16:31:12 +0100

    Implement routing with Navbar and Home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import './App.css';
 import ColorCombinations from './components/TextAndBackgroundCombination/ColorCombinations';
 import MultiStepForm from './components/MultiStepsForm/MultiStepForm';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/UI/Navbar';
 
-const App: React.FC = () => {
+function App() {
   return (
-    <>
-      <ColorCombinations />;
-      <MultiStepForm />;
-    </>
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/form" element={<MultiStepForm />} />
+        <Route path="/color" element={<ColorCombinations />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
