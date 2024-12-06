8eaf0112f47258158132ab171c210f72420eb648
Soroush <s.ghodrat@gmail.com>
2024-12-06 12:14:05 +0100

    Refactor App component layout and improve dark mode handling

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,36 @@ import Home from './pages/Home';
 import Navbar from './components/UI/Navbar';
 
 const App: React.FC = () => {
-    const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
 
-    useEffect(() => {
-      if (darkMode) {
-        document.documentElement.classList.add('dark');
-      } else {
-        document.documentElement.classList.remove('dark');
-      }
-    }, [darkMode]);
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
 
-    const toggleDarkMode = () => {
-      setDarkMode(!darkMode);
-    };
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
     <Router>
-      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/form" element={<MultiStepForm />} />
-        <Route path="/color" element={<ColorCombinations />} />
-      </Routes>
+      <div
+        className={`relative min-h-screen transition-bg ${darkMode ? 'bg-dark-gradient from-dark-gradient-start to-dark-gradient-end' : 'bg-light-gradient from-light-gradient-start to-light-gradient-end'}`}
+      >
+        <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+        <main className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/form" element={<MultiStepForm />} />
+            <Route path="/color" element={<ColorCombinations />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
-}
+};
 
 export default App;
